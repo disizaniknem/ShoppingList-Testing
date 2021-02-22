05e7a2c70419665796305fa5611f5a7bdcc1a693
disizaniknem <disizaniknem@gmail.com>
2021-02-22 21:42:51 +0100

    Testing ViewModels

--- app/src/main/java/com/disizaniknem/shoppinglisttesting/ui/ShoppingViewModel.kt
+++ app/src/main/java/com/disizaniknem/shoppinglisttesting/ui/ShoppingViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.disizaniknem.shoppinglisttesting.data.local.ShoppingItem
 import com.disizaniknem.shoppinglisttesting.data.remote.responses.ImageResponse
+import com.disizaniknem.shoppinglisttesting.others.Constants
 import com.disizaniknem.shoppinglisttesting.others.Event
 import com.disizaniknem.shoppinglisttesting.others.Resource
 import com.disizaniknem.shoppinglisttesting.repositories.ShoppingRepository
@@ -27,7 +28,8 @@ class ShoppingViewModel @ViewModelInject constructor(
     val curImageUrl: LiveData<String> = _curImageUrl
 
     private val _insertShoppingItemStatus = MutableLiveData<Event<Resource<ShoppingItem>>>()
-    val insertShoppingItemStatus: LiveData<Event<Resource<ShoppingItem>>> = _insertShoppingItemStatus
+    val insertShoppingItemStatus: LiveData<Event<Resource<ShoppingItem>>> =
+        _insertShoppingItemStatus
 
     fun setCurImageUrl(url: String) {
         _curImageUrl.postValue(url)
@@ -42,12 +44,60 @@ class ShoppingViewModel @ViewModelInject constructor(
     }
 
     fun insertShoppingItem(name: String, amountString: String, price: String) {
+        if (name.isEmpty() || amountString.isEmpty() || price.isEmpty()) {
+            _insertShoppingItemStatus.postValue(
+                Event(
+                    Resource.error(
+                        "The fields must not be empty",
+                        null
+                    )
+                )
+            )
+            return
+        }
+        if (name.length > Constants.MAX_NAME_LENGTH) {
+            _insertShoppingItemStatus.postValue(
+                Event(
+                    Resource.error(
+                        "The name of the item must not exceec ${Constants.MAX_NAME_LENGTH} characters",
+                        null
+                    )
+                )
+            )
+            return
+        }
+        if (price.length > Constants.MAX_PRICE_LENGTH) {
+            _insertShoppingItemStatus.postValue(
+                Event(
+                    Resource.error(
+                        "The price of the item must not exceec ${Constants.MAX_PRICE_LENGTH} characters",
+                        null
+                    )
+                )
+            )
+            return
+        }
+        val amount = try {
+            amountString.toInt()
+        } catch (e:Exception) {
+            _insertShoppingItemStatus.postValue(Event(Resource.error("Please enter a valid amount", null)))
+            return
+        }
+        val shoppingItem = ShoppingItem(name, amount, price.toFloat(), _curImageUrl.value ?: "")
+        insertShoppingItemIntoDb(shoppingItem)
+        setCurImageUrl("")
+        _insertShoppingItemStatus.postValue(Event(Resource.success(shoppingItem)))
 
     }
 
     fun searchForImage(imageQuery: String) {
-
+        if (imageQuery.isEmpty()) {
+            return
+        }
+        _images.value = Event(Resource.loading(null))
+        viewModelScope.launch {
+            val response = repository.searchForImage(imageQuery)
+            _images.value = Event(response)
+        }
     }
-
-
 }
---
+++ app/src/test/java/com/disizaniknem/shoppinglisttesting/ui/ShoppingViewModelTest.kt
@@ -0,0 +1,88 @@
+package com.disizaniknem.shoppinglisttesting.ui
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.disizaniknem.shoppinglisttesting.MainCoroutineRule
+import com.disizaniknem.shoppinglisttesting.getOrAwaitValueTest
+import com.disizaniknem.shoppinglisttesting.others.Constants
+import com.disizaniknem.shoppinglisttesting.others.Status
+import com.disizaniknem.shoppinglisttesting.repositories.FakeShoppingRepository
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class ShoppingViewModelTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    private lateinit var viewModel: ShoppingViewModel
+
+    @Before
+    fun setup() {
+        viewModel = ShoppingViewModel(FakeShoppingRepository())
+    }
+
+    @Test
+    fun `insert shopping item with empty field, returns error` () {
+        viewModel.insertShoppingItem("name", "", "3.0")
+
+        val value = viewModel.insertShoppingItemStatus.getOrAwaitValueTest()
+
+        assertThat(value.getContentIfNotHandled()?.status).isEqualTo(Status.ERROR)
+    }
+
+    @Test
+    fun `insert shopping item with too long name, returns error` () {
+        val longName = buildString {
+            for (i in 1..Constants.MAX_NAME_LENGTH + 1) {
+                append(1)
+            }
+        }
+        viewModel.insertShoppingItem(longName, "5", "3.0")
+
+        val value = viewModel.insertShoppingItemStatus.getOrAwaitValueTest()
+
+        assertThat(value.getContentIfNotHandled()?.status).isEqualTo(Status.ERROR)
+    }
+
+    @Test
+    fun `insert shopping item with too long price, returns error` () {
+        val longPrice = buildString {
+            for (i in 1..Constants.MAX_PRICE_LENGTH + 1) {
+                append(1)
+            }
+        }
+        viewModel.insertShoppingItem("name", "5", longPrice)
+
+        val value = viewModel.insertShoppingItemStatus.getOrAwaitValueTest()
+
+        assertThat(value.getContentIfNotHandled()?.status).isEqualTo(Status.ERROR)
+    }
+
+    @Test
+    fun `insert shopping item with too high amount, returns error` () {
+
+        viewModel.insertShoppingItem("name", "999999999999999999999999999999", "3.0")
+
+        val value = viewModel.insertShoppingItemStatus.getOrAwaitValueTest()
+
+        assertThat(value.getContentIfNotHandled()?.status).isEqualTo(Status.ERROR)
+    }
+
+    @Test
+    fun `insert shopping item with valid input, returns success` () {
+
+        viewModel.insertShoppingItem("name", "5", "3.0")
+
+        val value = viewModel.insertShoppingItemStatus.getOrAwaitValueTest()
+
+        assertThat(value.getContentIfNotHandled()?.status).isEqualTo(Status.SUCCESS)
+    }
+
+}
