22120cec3f6c030dc0ac249631957871269a99e0
disizaniknem <disizaniknem@gmail.com>
2021-02-24 23:50:52 +0100

    Test adding of shopping item

--- app/src/androidTest/java/com/disizaniknem/shoppinglisttesting/ui/AddShoppingItemFragmentTest.kt
+++ app/src/androidTest/java/com/disizaniknem/shoppinglisttesting/ui/AddShoppingItemFragmentTest.kt
@@ -1,23 +1,32 @@
 package com.disizaniknem.shoppinglisttesting.ui
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.MediumTest
 import com.disizaniknem.shoppinglisttesting.R
+import com.disizaniknem.shoppinglisttesting.data.local.ShoppingItem
+import com.disizaniknem.shoppinglisttesting.getOrAwaitValue
 import com.disizaniknem.shoppinglisttesting.launchFragmentInHiltContainer
+import com.disizaniknem.shoppinglisttesting.repositories.FakeShoppingRepositoryAndroidTest
+import com.google.common.truth.Truth.assertThat
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
+import javax.inject.Inject
 
 @MediumTest
 @HiltAndroidTest
@@ -25,11 +34,33 @@ import org.mockito.Mockito.verify
 class AddShoppingItemFragmentTest {
 
     @get:Rule
-    var hiltRule = HiltAndroidRule(this)
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var fragmentFactory: ShoppingFragmentFactory
 
     @Before
     fun setup() {
         hiltRule.inject()
+    }
+
+    @Test
+    fun clickInsertIntoDb_shoppingItemInsertedIntoDb() {
+        val testViewModel = ShoppingViewModel(FakeShoppingRepositoryAndroidTest())
+        launchFragmentInHiltContainer<AddShoppingItemFragment>(fragmentFactory = fragmentFactory) {
+            viewModel = testViewModel
+        }
+
+        onView(withId(R.id.etShoppingItemName)).perform(replaceText("shopping item"))
+        onView(withId(R.id.etShoppingItemAmount)).perform(replaceText("5"))
+        onView(withId(R.id.etShoppingItemPrice)).perform(replaceText("3.3"))
+        onView(withId(R.id.btnAddShoppingItem)).perform(click())
+
+        assertThat(testViewModel.shoppingItems.getOrAwaitValue())
+            .contains(ShoppingItem("shopping item", 5, 3.3f, "") )
     }
 
     @Test
